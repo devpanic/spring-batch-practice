23b6be777f0ac5220c4f4ddeed9513c0771fea58
devpanic <yeon.dev.8@gmail.com>
2024-09-29 23:47:10 +0900

    Initial Commit

---
+++ src/main/java/com/devpanic/batchprj/BatchPrjApplication.java
@@ -0,0 +1,13 @@
+package com.devpanic.batchprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchPrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devpanic/batchprj/BatchPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpanic.batchprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
